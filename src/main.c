fa686665a2469c49e326a8acb01e96ebca9cd49a
Carlos Daniel <carlos.pasquali.dev@gmail.com>
2024-11-25 19:18:24 -0300

    final project

--- src/main.c
+++ src/main.c
@@ -1,100 +1,74 @@
+// CARLOS PASQUALI, HENRIQUE JOCHEM
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/pcnt.h"
 #include "driver/gpio.h"
-#include "esp_log.h"
-#include "driver/i2c.h"
-#include "bmp180.h"
 
-// CARLOS PASQUALI, HENRIQUE JOCHEM
-#include "ssd1306.h"
+// ENCODER
+#define PCNT_INPUT_SIG_IO 4  // Pino de entrada do sinal de pulso
+#define PCNT_CTRL_IO 5       // Pino de controle do contador
+#define PCNT_H_LIM_VAL 1000  // Limite superior de contagem
+#define PCNT_L_LIM_VAL -1000 // Limite inferior de contagem
+int16_t count = 0;
 
-static const char *TAG = "BMP180 I2C Read";
-
-#define REFERENCE_PRESSURE 101325l
-
-// SENSOR
-#define I2C_PIN_SDA 21
-#define I2C_PIN_SCL 22
-
-float altitude;
-float temperature;
-char lineChar[20];
-
-// OLED
-#define OLED_SDA_GPIO 21   // define the SDA GPIO
-#define OLED_SCL_GPIO 22   // define the SCL GPIO
-#define OLED_RESET_GPIO -1 // define the RESET GPIO
-// OLED CONFIGURATION
-SSD1306_t oled;
-
-/*void bmp180_task(void *pvParameter)
+void config_pulse_counter()
 {
-}*/
+    // Configurações iniciais para o contador de pulsos
+    pcnt_config_t pcnt_config = {
+        .pulse_gpio_num = PCNT_INPUT_SIG_IO, // Pino de entrada de pulsos
+        .ctrl_gpio_num = PCNT_CTRL_IO,       // Pino de controle
+        .channel = PCNT_CHANNEL_0,           // Canal do contador
+        .unit = PCNT_UNIT_0,                 // Unidade do contador
+        .pos_mode = PCNT_COUNT_INC,          // Contagem positiva (borda de subida)
+        .neg_mode = PCNT_COUNT_DIS,          // Desabilitar contagem negativa (borda de descida)
+        .counter_h_lim = PCNT_H_LIM_VAL,     // Limite superior
+        .counter_l_lim = PCNT_L_LIM_VAL,     // Limite inferior
+        .lctrl_mode = PCNT_MODE_KEEP,        // Mantém o estado se o pino de controle estiver em nível baixo
+        .hctrl_mode = PCNT_MODE_REVERSE      // Mantém o estado se o pino de controle estiver em nível alto
+    };
+
+    // Configuração do contador
+    pcnt_unit_config(&pcnt_config);
+    // Inicializa o contador, pausa, e reseta para zero
+    pcnt_counter_pause(PCNT_UNIT_0); // Pausar o contador
+    pcnt_counter_clear(PCNT_UNIT_0); // Limpar o valor do contador
+    // Definir o limite de eventos do contador (opcional)
+    pcnt_set_event_value(PCNT_UNIT_0, PCNT_EVT_H_LIM, PCNT_H_LIM_VAL); // Evento no limite superior
+    // Reinicia a contagem
+    pcnt_counter_resume(PCNT_UNIT_0); // Retomar a contagem de pulsos
+}
+
+// FAN
+#define FAN 23 // Pino onde o LED está conectado
+
+void setup()
+{
+    // FAN
+    gpio_reset_pin(FAN);
+    gpio_set_direction(FAN, GPIO_MODE_OUTPUT);
+
+    // ENCODER
+    config_pulse_counter();
+}
 
 void app_main()
 {
+    // ENCODER
 
-    ESP_LOGI(TAG, "Panel is 128x64");
-    ESP_LOGI(TAG, "INTERFACE is i2c");
-    ESP_LOGI(TAG, "CONFIG_SDA_GPIO=%d", OLED_SDA_GPIO);
-    ESP_LOGI(TAG, "CONFIG_SCL_GPIO=%d", OLED_SCL_GPIO);
-    ESP_LOGI(TAG, "CONFIG_RESET_GPIO=%d", OLED_RESET_GPIO);
-    i2c_master_init(&oled, OLED_SDA_GPIO, OLED_SCL_GPIO, OLED_RESET_GPIO); // initialize the i2c master driver
-
-    // initialize the OLED
-    ssd1306_init(&oled, 128, 64);                          // initialize the OLED
-    ssd1306_clear_screen(&oled, false);                    // clear the OLED
-    ssd1306_contrast(&oled, 0xFF);                         // set the contrast
-    ssd1306_display_text_x3(&oled, 0, "BMP180", 5, false); // display the text
-    vTaskDelay(pdMS_TO_TICKS(3000));                       // delay
-
-    ssd1306_clear_screen(&oled, false); // clear the OLED
-    ssd1306_contrast(&oled, 0xFF);      // set the contrast
-
-    esp_err_t err; // init error
-
-    err = bmp180_init(I2C_PIN_SDA, I2C_PIN_SCL);
-    if (err == ESP_OK)
+    while (1)
     {
-        // xTaskCreate(&bmp180_task, "bmp180_task", 1024 * 4, NULL, 5, NULL);
-        while (1)
-        {
-            esp_err_t err;
-            uint32_t pressure;
+        // ENCODER
+        //  Ler o valor atual do contador
+        pcnt_get_counter_value(PCNT_UNIT_0, &count);
+        printf("Valor do encoder: %d\n", count);
+        // Delay para a próxima leitura (por exemplo, a cada segundo)
+        vTaskDelay(pdMS_TO_TICKS(1000));
 
-            err = bmp180_read_pressure(&pressure);
-            if (err != ESP_OK)
-            {
-                ESP_LOGE(TAG, "Reading of pressure from BMP180 failed, err = %d", err);
-            }
-            err = bmp180_read_altitude(REFERENCE_PRESSURE, &altitude);
-            if (err != ESP_OK)
-            {
-                ESP_LOGE(TAG, "Reading of altitude from BMP180 failed, err = %d", err);
-            }
-            err = bmp180_read_temperature(&temperature);
-            if (err != ESP_OK)
-            {
-                ESP_LOGE(TAG, "Reading of temperature from BMP180 failed, err = %d", err);
-            }
-            ESP_LOGI(TAG, "Pressure %lu Pa, Altitude %.1f m, Temperature : %.1f degC", pressure, altitude, temperature);
-
-            // OLED
-            snprintf(lineChar, sizeof(lineChar), "Alt:  %.1f m", altitude);
-            ssd1306_display_text(&oled, 0, lineChar, sizeof(lineChar), false);
-
-            snprintf(lineChar, sizeof(lineChar), "Temp: %.1f C", temperature);
-            ssd1306_display_text(&oled, 1, lineChar, sizeof(lineChar), false);
-
-            snprintf(lineChar, sizeof(lineChar), "Pres: %lu Pa", pressure);
-            ssd1306_display_text(&oled, 2, lineChar, sizeof(lineChar), false);
-
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
-    }
-    else
-    {
-        ESP_LOGE(TAG, "BMP180 init failed with error = %d", err);
+        // FAN
+        gpio_set_level(FAN, 1);
+        // vTaskDelay(5000 / portTICK_PERIOD_MS);
+        // gpio_set_level(FAN, 0);
+        // vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
 }
